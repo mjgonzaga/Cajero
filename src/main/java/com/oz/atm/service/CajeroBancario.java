92de931afc80498f46642d8d552a8d4454ea235a
leonel <leonel@leonel-SVE14A15FLW>
2014-06-14 09:58:39 -0500

    #2:version

---
+++ src/main/java/com/oz/atm/service/CajeroBancario.java
@@ -0,0 +1,8 @@
+package com.oz.atm.service;
+
+
+
+public class CajeroBancario {
+    }
+
+   
--- src/main/java/com/utpl/cajeroat/Main.java
+++ src/main/java/com/utpl/cajeroat/Main.java
@@ -10,9 +10,21 @@ package com.utpl.cajeroat;
  *
  * @author leonel
  */
+
+
+import com.oz.atm.gui.CajeroSwing;
+import com.oz.atm.gui.Principal;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
-    public static void main(String [] args) {
-        
+
+    public static void main(String[] args) {
+        Principal pri=new Principal();
+        pri.setVisible(true);
+
+     
     }
     
 }
